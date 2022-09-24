1da9e8423245401f300b4ee40cf6834666fbe88f
dooreelko <sascha.fedorenko@pm.me>
2022-09-24 10:53:19 +0200

    more eager copying of sub-dependencies

--- modules/tools/dep-copy/src/main.ts
+++ modules/tools/dep-copy/src/main.ts
@@ -4,6 +4,13 @@ import * as path from 'path';
 import * as fs from 'fs';
 import copy from 'cpy';
 
+/**
+ * This thing is intended to detect and copy only prod dependencies of a module.
+ * TODO: use the npm tree to find all modules
+ * 
+ * right now it only reliably skips top-level non-prod modules
+ */
+
 if (process.argv.length < 4) {
     console.error(`dep-copy <src_dir> <dst_dir>. 
 Recursively copy only production parts of node_modules`);
@@ -14,39 +21,67 @@ const from = process.argv[2];
 const into = process.argv[3];
 
 type PkgJsonWithDeps = { dependencies?: Record<string, string> };
+type ToCopy = { source: string[], dest: string };
 
-const processDir = async (dir: string) => {
+const processDir: (dir: string) => ToCopy[] = (dir: string) => {
+    console.error(`checking ${dir}`);
     const pkgPath = path.join(dir, 'package.json');
     if (!fs.existsSync(pkgPath)) {
-        return;
+        // console.error(`No package.json in ${pkgPath}`);
+        return [];
     }
+
+    let selfs: ToCopy[] = [];
 
     if (from !== dir) {
         const fromGlob = path.join(dir, '**');
         const notGlob = path.join(dir, 'node_modules');
         const intoDest = path.join(into, dir);
-        console.error(`will copy from ${fromGlob} but not ${notGlob} into ${intoDest}`);
+        // console.error(`will copy from ${fromGlob} but not ${notGlob} into ${intoDest}`);
+        // console.error(`${fromGlob} -> ${intoDest}`);
 
-        await copy([
-            fromGlob,
-            `!${notGlob}`
-        ], intoDest, {
-            ignoreJunk: true
-        });
+        selfs = [{
+            source: [
+                fromGlob
+                // `!${notGlob}`
+            ],
+            dest: intoDest
+        }];
     }
 
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const pkgJson = JSON.parse(fs.readFileSync(pkgPath, 'utf-8')) as PkgJsonWithDeps;
 
     if (!pkgJson.dependencies) {
-        return;
+        console.error(`no deps in ${pkgPath}`);
+        return selfs;
     }
 
-    Object
-        .keys(pkgJson.dependencies)
-        .map(pkg => path.join(dir, 'node_modules', pkg))
-        .map(processDir);
+    return [
+        ...selfs,
+        ...Object
+            .keys(pkgJson.dependencies)
+            .map(pkg => path.join(dir, 'node_modules', pkg))
+            .flatMap(processDir)
+    ];
 };
 
-await processDir(from)
-    .catch(console.error);
+await Promise.all(
+    processDir(from)
+        .map(tocopy => {
+            console.error(tocopy.source[0]);
+
+            return tocopy;
+        })
+        .map(tocopy =>
+            copy(tocopy.source, tocopy.dest, {
+                ignoreJunk: true,
+                filter: srcFile => {
+                    // console.error(`copying ${srcFile.relativePath}`);
+
+                    return true;
+                }
+            })
+        )
+)
+    // .catch(console.error);
