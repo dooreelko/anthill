5e023dff93234f465ef6fd9ea29d3a88cd193049
dooreelko <sascha.fedorenko@pm.me>
2022-10-30 22:04:11 +0100

    [ticketing|-] feature kind and doc ref in changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,9 @@
 # Arinoto changelog
 
-## 2022-10-30 Ticketing within repo
+## 2022-10-30 [feature]  Ticketing within repo
 
 Design specifications and ticketing integrated in the project’s
 repository.
+[More information](<spec/000 ticketing/design.md>)
+
 
--- ci/changelog.sh
+++ ci/changelog.sh
@@ -12,6 +12,9 @@ source "$SCRIPT_DIR/helpers.sh"
 STORY=$(feature-story)
 export STORY
 
+STORY_KIND=$(feature-kind)
+export STORY_KIND
+
 STORY_DIR="$(feature-dir "$STORY")"
 
 if [ -z "$STORY_DIR" ]; then
@@ -23,7 +26,11 @@ STORY_DOC="$STORY_DIR/design.md"
 
 if [ ! -f "$STORY_DOC" ]; then
     echo "$STORY_DOC file does not exist."
+    exit 1
 fi
+
+STORY_DOC_REL=$(realpath --relative-to "$ROOT_DIR" "$STORY_DOC")
+export STORY_DOC_REL
 
 NOW=$(date +%Y-%m-%d)
 
@@ -37,7 +44,7 @@ MD=$(pandoc "$STORY_DOC" -t json \
         meta: .meta}' \
     | pandoc -f json -t gfm)
     
-HEAD=$(echo "$MD" | head -n 1 | sed "s/^#/## $NOW/")
+HEAD=$(echo "$MD" | head -n 1 | sed "s/^#/$NOW [$STORY_KIND] /")
 export HEAD
 
 TAIL=$(echo "$MD" | tail -n +2)
@@ -47,7 +54,8 @@ NEW_CL=$(jq '[.[] | select(.story != env.STORY)] | . as $rest | [
 {
     story: env.STORY,
     title: env.HEAD,
-    desc: env.TAIL
+    desc: env.TAIL,
+    doc: env.STORY_DOC_REL
 }] + .' "$ROOT_DIR/changelog.json")
 
 (
@@ -55,10 +63,10 @@ NEW_CL=$(jq '[.[] | select(.story != env.STORY)] | . as $rest | [
     echo
     echo "$NEW_CL" \
         | tee "$ROOT_DIR/changelog.json" \
-        | jq -r '.[] | .title + "\n" + .desc + "\n"'
+        | jq -r '.[] | "## " + .title + "\n" + .desc + "\n[More information](<" + .doc + ">)\n\n"'
 ) > "$ROOT_DIR/CHANGELOG.md"
 
 if ! git diff --exit-code "$ROOT_DIR/CHANGELOG.md"; then 
-    git add "$ROOT_DIR/CHANGELOG.md" "$ROOT_DIR/CHANGELOG.json"
+    git add "$ROOT_DIR/CHANGELOG.md" "$ROOT_DIR/changelog.json"
     git commit --amend --reuse-message=HEAD --no-verify
 fi
